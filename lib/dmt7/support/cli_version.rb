1b6e6bb5c73cc476c986b00d8b84a112df93af7e
Donovan Young <dyoung522@gmail.com>
2023-09-20 12:14:22 -0400

    Add descriptions to cli options

--- lib/dmt7/support/cli_version.rb
+++ lib/dmt7/support/cli_version.rb
@@ -4,13 +4,22 @@ require "thor"
 
 module DMT7
   class CLIversion < Thor
-    class_option :verbose, type: :boolean, aliases: "-v"
-    class_option :dry_run, type: :boolean
+    class_option :verbose,
+                 type: :boolean,
+                 aliases: "-v",
+                 desc: "Prints more verbose output"
+    class_option :dry_run,
+                 type: :boolean,
+                 desc: "Won't actually perform any destructive operations (e.g. writing to files)"
 
     desc "bump MODLET_PATH", "Bumps the version included in the MODLET_PATH/ModInfo.xml file"
-    option :major, type: :numeric, aliases: "-M"
-    option :minor, type: :numeric, aliases: "-m"
-    option :patch, type: :numeric, aliases: "-p"
+    long_desc <<-LONGDESC
+      `dmt7 version bump MODLET_PATH` will bump the version included in the MODLET_PATH/ModInfo.xml file.\n
+      If no version options are provided, the patch version will be bumped by default.
+    LONGDESC
+    option :major, type: :numeric, aliases: "-M", desc: "Specify the major version"
+    option :minor, type: :numeric, aliases: "-m", desc: "Specify the minor version"
+    option :patch, type: :numeric, aliases: "-p", desc: "Specify the patch version"
     def bump(modlet_path)
       Plugins::Version.new(modlet_path, options).bump.save
     rescue DMT7error => e
